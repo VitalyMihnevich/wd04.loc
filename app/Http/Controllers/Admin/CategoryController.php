8d12d220f8ac7a2d7d9ab437d893fafdaa3d237d
VitalyMikhnevich <vitaly14115@gmail.com>
2022-04-24 19:15:58 +0300

    add: Добавление категорий товара.

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        return view('admin.categories.index', [
+            'categories'=>Category::paginate()
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function create()
+    {
+        $categories = Category::where('parent_id', 0)->get();
+        return view('admin.categories.create',compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreCategoryRequest  $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        Category::create($request->all());
+        return redirect(route('category.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        $categories = Category::where('parent_id', 0)->get();
+        return view('admin.categories.edit', compact('category', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+          * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreCategoryRequest $request, Category $category)
+    {
+        $category->fill($request->all())->save();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Category  $category
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()->route('category.index');
+
+    }
+}
